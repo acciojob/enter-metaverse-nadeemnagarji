7659af0c72f3bc91525f29c84504a9747bea846f
nadeemnagarji <129616695+nadeemnagarji@users.noreply.github.com>
2023-09-17 20:20:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const para = document.getElementById("status")
+function changeText(){
+	const heading = document.createElement("h1")
+	heading.innerText = "Entered Metaverse"
+	para.replaceWith(heading)
+}
